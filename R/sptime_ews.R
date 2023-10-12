ff4e986b499731b2f268f25c0ecb2555ce8eb18a
alibakhs <sara.alibakhshi@gmail.com>
2023-10-12 22:51:20 +0300

    update

--- R/sptime_ews.R
+++ R/sptime_ews.R
@@ -9,7 +9,7 @@
     ex<-na.omit(ex1)
     if (length(ex) > 10) {
       #ex1<-na.omit(ex) 
-      kt<-trend::mk.test(ex)
+      kt<-mk.test(ex)
       ktt<-kt$estimates[3]
       ktt
     } else NA
@@ -119,7 +119,7 @@
           if (all(is.na(x))) {
             return(NA)
           } else {
-            x <- imputeTS::na.ma(x,k=k)
+            x <- na.ma(x,k=k)
             a <- .ac(x,r=rw)
             if (!is.null(a)) a$tau
             else NA
@@ -135,7 +135,7 @@
           if (all(is.na(x))) {
             return(NA)
           } else {
-            x <- imputeTS::na.ma(x,k=k)
+            x <- na.ma(x,k=k)
             x <- .gauss.detrend(x, bw = bw, timeindex = timeindex)
             a <- .ac(x,r=rw)
             if (!is.null(a)) a$tau
@@ -150,7 +150,7 @@
           if (all(is.na(x))) {
             return(NA)
           } else {
-            x <- imputeTS::na.ma(x,k=k)
+            x <- na.ma(x,k=k)
             x <- .fdif.detrend(x)
             
             x<-rollapply(x, width=6, FUN=mean, na.rm = TRUE, fill = NA)
@@ -216,6 +216,171 @@
 }
 #----------
 
+.sptime_ews.terra <- function(x, detrend=NULL,bw=5, interpolate=NULL,k=6,rw=50) {
+  bw <- round(nlyr(x@raster) * bw / 100)
+  rw <- round(nlyr(x@raster) * rw / 100)
+  
+  timeindex <- index(x[1])
+  
+  if (!is.null(interpolate)) {
+    if (interpolate == "approx") {
+      if (is.null(detrend)) {
+        #---------
+        cat('\n no detrending, approx for interpolation, procedure is started for raster time series')
+        .f <- function(x,...) {
+          if (all(is.na(x))) {
+            return(NA)
+          } else {
+            x <- na.approx(x)
+            a <- .ac(x,r=rw)
+            if (!is.null(a)) a$tau
+            else NA
+          }
+        }
+        #-------------------
+        app(x@raster,.f)
+        
+        
+      } else if (detrend == "gaussian") {
+        cat('\n gaussian detrending, approx for interpolation, procedure is started for raster time series')
+        .f <- function(x,...) {
+          if (all(is.na(x))) {
+            return(NA)
+          } else {
+            x <- na.approx(x)
+            x <- .gauss.detrend(x, bw = bw, timeindex = timeindex)
+            a <- .ac(x,r=rw)
+            if (!is.null(a)) a$tau
+            else NA
+          }
+        }
+        #-------------------
+        app(x@raster,.f)
+      } else if (detrend == "fdif") {
+        cat('\n fdif detrending, approx for interpolation, procedure is started for raster time series')
+        .f <- function(x,...) {
+          if (all(is.na(x))) {
+            return(NA)
+          } else {
+            x <- na.approx(x)
+            x <- .fdif.detrend(x)
+            a <- .ac(x,r=rw)
+            if (!is.null(a)) a$tau
+            else NA
+          }
+        }
+        #-------------------
+        app(x@raster,.f)
+        
+        
+        
+      }
+      
+      
+    } else if (interpolate == "ma") {
+      if (is.null(detrend)) {
+        #---------
+        cat('\n no detrending, ma for interpolation, procedure is started for raster time series')
+        .f <- function(x,...) {
+          if (all(is.na(x))) {
+            return(NA)
+          } else {
+            x <- na.ma(x,k=k)
+            a <- .ac(x,r=rw)
+            if (!is.null(a)) a$tau
+            else NA
+          }
+        }
+        #-------------------
+        app(x@raster,.f)
+        
+        
+      } else if (detrend == "gaussian") {
+        cat('\n gaussian detrending, ma for interpolation, procedure is started for raster time series')
+        .f <- function(x,...) {
+          if (all(is.na(x))) {
+            return(NA)
+          } else {
+            x <- na.ma(x,k=k)
+            x <- .gauss.detrend(x, bw = bw, timeindex = timeindex)
+            a <- .ac(x,r=rw)
+            if (!is.null(a)) a$tau
+            else NA
+          }
+        }
+        #-------------------
+        app(x@raster,.f)
+      } else if (detrend == "fdif") {
+        cat('\n fdif detrending, ma for interpolation, procedure is started for raster time series')
+        .f <- function(x,...) {
+          if (all(is.na(x))) {
+            return(NA)
+          } else {
+            x <- na.ma(x,k=k)
+            x <- .fdif.detrend(x)
+            
+            x<-rollapply(x, width=6, FUN=mean, na.rm = TRUE, fill = NA)
+            a <- .ac(x,r=rw)
+            if (!is.null(a)) a$tau
+            else NA
+          }
+        }
+        #-------------------
+        app(x@raster,.f)
+        
+      }
+    }
+  } else {
+    if (is.null(detrend)) {
+      #---------
+      cat('\n no detrending, no interpolation, procedure is started for raster time series')
+      .f <- function(x,...) {
+        if (all(is.na(x))) {
+          return(NA)
+        } else {
+          a <- .ac(x,r=rw)
+          if (!is.null(a)) a$tau
+          else NA
+        }
+      }
+      #-------------------
+      app(x@raster,.f)
+      
+      
+    } else if (detrend == "gaussian") {
+      cat('\n gaussian detrending, no interpolation, procedure is started for raster time series')
+      .f <- function(x,...) {
+        if (all(is.na(x))) {
+          return(NA)
+        } else {
+          x <- .gauss.detrend(x, bw = bw, timeindex = timeindex)
+          a <- .ac(x,r=rw)
+          if (!is.null(a)) a$tau
+          else NA
+        }
+      }
+      #-------------------
+      app(x@raster,.f)
+    } else if (detrend == "fdif") {
+      cat('\n fdif detrending, no interpolation, procedure is started for raster time series')
+      .f <- function(x,...) {
+        if (all(is.na(x))) {
+          return(NA)
+        } else {
+          x <- .fdif.detrend(x)
+          a <- .ac(x,r=rw)
+          if (!is.null(a)) a$tau
+          else NA
+        }
+      }
+      #-------------------
+      app(x@raster,.f)
+      
+    }
+  }
+  
+}
+#----------
 
 
 #--------
@@ -229,7 +394,13 @@ if (!isGeneric("sptime_ews")) {
 
 setMethod('sptime_ews', signature(x='RasterStackBrickTS'),
           function(x, detrend=NULL,bw=5, interpolate=NULL,k=6,rw=50) {
-            .sptime_ews(x, detrend=NULL,bw=5, interpolate=NULL,k=6,rw=50)
+            .sptime_ews(x,  detrend=detrend,bw=bw, interpolate=interpolate,k=k,rw=rw)
           }
 )
 
+
+setMethod('sptime_ews', signature(x='SpatRasterTS'),
+          function(x, detrend=NULL,bw=5, interpolate=NULL,k=6,rw=50) {
+            .sptime_ews.terra(x, detrend=detrend,bw=bw, interpolate=interpolate,k=k,rw=rw)
+          }
+)
--- R/stew.R
+++ R/stew.R
@@ -1,12 +1,50 @@
 
 # measure local indicators of spatial association (LISA) using the package elsa
 .getLisa <- function(x,d1,d2,stat) {
-  elsa::lisa(x,d1,d2,statistic = stat)
+  lisa(x,d1,d2,statistic = stat)
 }
-
+#---
+.rasKendal <- function(x,pv_sig=0.05,...) {
+  # based on the mk.test function in the package trend
+  if (all(is.na(x))) return(NA)
+  
+  x <- x[!is.na(x)]
+  
+  n <- length(x)
+  
+  if(n < 3) stop("'x' must have at least 3 elements!")
+  
+  S <- 0.0
+  for(j in 1:n) S <- S + sum(sign(x[j] - x[1:j]))
+  ## get ties
+  t <- table(x)
+  names(t) <- NULL
+  
+  tadjs <- sum(t * (t - 1) * (2 * t + 5))
+  varS <- (n * (n-1) * (2 * n + 5) - tadjs) / 18
+  
+  tadjd <- sum(t * (t - 1))
+  D <- sqrt(1/2 * n * (n - 1) - 1/2 * tadjd) * sqrt(1/2 * n * (n - 1))
+  tau <- S / D
+  
+  ## compute z
+  sg <- sign(S)
+  z <- sg * (abs(S) - 1) / sqrt(varS)
+  
+  
+  ## get the pvalue
+  pval <- 2 * min(0.5, pnorm(abs(z), lower.tail=FALSE)) # two.sided
+  
+  if (!is.null(pv_sig) && !is.na(pval)) {
+    if (pval > pv_sig) tau <- NA
+  }
+  
+  tau
+  
+}
 #--------
 if (!isGeneric("stew")) {
-  setGeneric("stew", function(x,stat,d,output,...)
+  setGeneric("stew", function(x,stat,d,output,pv_sig,...)
     standardGeneric("stew"))
 }
 
@@ -14,11 +52,12 @@ if (!isGeneric("stew")) {
 
 
 setMethod('stew', signature(x='RasterStackBrickTS'),
-          function(x,stat,d,output,...) {
+          function(x,stat,d,output,pv_sig,...) {
             ind <- index(x)
-            .terra <- require('terra',character.only = TRUE,quietly = TRUE)
-
+            
             k <- NULL
+            
+            if (missing(pv_sig)) pv_sig <- NULL
 
             if (missing(stat)) stat <- 'stg'
             else stat <- tolower(stat)
@@ -26,16 +65,18 @@ setMethod('stew', signature(x='RasterStackBrickTS'),
             xx <- x@raster
 
             if (missing(d)) d <- max(res(xx)) * sqrt(2)
-
-
-            # if (require(terra)) {
-            #   xx <- rast(x@raster)
-            # } else {
-            #   xx <- x@raster
-            # }
-            #if (ncell(xx) > 1e7)
-
-            o <- raster(xx)
+            
+            if (missing(output)) output <- 'rts'
+            else {
+              if (!all(output %in% c('raster','rts','tau'))) {
+                warning('Not all the specified items in output is recognized, default "rts" is used')
+                output <- 'rts'
+              }
+            }
+            
+            
+            
+            o <- raster(xx[[1]])
 
             if (stat %in% c('stg','geary','localgeary','c')) {
               for (i in 1:nlayers(xx)) {
@@ -48,11 +89,67 @@ setMethod('stew', signature(x='RasterStackBrickTS'),
             }
             #-------
             if ('tau' %in% output) {
-              if (.terra) {
-                k <- app(rast(o),.getKendal)
-              } else {
-                k <- calc(o,.getKendal)
+              k <- calc(o,.rasKendal,pv_sig=pv_sig)
+            }
+            #--------
+            if ('rts' %in% output) {
+              o <- rts(o, ind)
+            }
+            #--------
+            if (length(output) == 1) {
+              if (output == 'tau') return(k)
+              else return(o)
+            } else {
+              oo <- list()
+              if ('tau' %in% output) oo[['tau']] <- k
+              if ('rts' %in% output) oo[['rts']] <- o
+              else oo[['raster']] <- o
+              return (oo)
+            }
+
+          }
+)
+#------
+
+setMethod('stew', signature(x='SpatRasterTS'),
+          function(x,stat,d,output,pv_sig,...) {
+            ind <- index(x)
+            
+            k <- NULL
+            
+            if (missing(pv_sig)) pv_sig <- NULL
+            
+            if (missing(stat)) stat <- 'stg'
+            else stat <- tolower(stat)
+            
+            xx <- x@raster
+            
+            if (missing(d)) d <- max(res(xx)) * sqrt(2)
+            
+            if (missing(output)) output <- 'rts'
+            else {
+              if (!all(output %in% c('raster','rts','tau'))) {
+                warning('Not all the specified items in output is recognized, default "rts" is used')
+                output <- 'rts'
               }
+            }
+            
+            
+            
+            o <- rast(xx[[1]])
+            
+            if (stat %in% c('stg','geary','localgeary','c')) {
+              for (i in 1:nlyr(xx)) {
+                o <- c(o, .getLisa(xx[[i]],d1=0,d2=d,stat='c'))
+              }
+            } else if (stat %in% c('stm','moran','localmoran','m','i')) {
+              for (i in 1:nlyr(xx)) {
+                o <- c(o, .getLisa(xx[[i]],d1=0,d2=d,stat='i'))
+              }
+            }
+            #-------
+            if ('tau' %in% output) {
+                k <- app(rast(o),.rasKendal,pv_sig=pv_sig)
             }
             #--------
             if (c('rts') %in% output) {
@@ -69,6 +166,6 @@ setMethod('stew', signature(x='RasterStackBrickTS'),
               else oo[['raster']] <- o
               return (oo)
             }
-
+            
           }
 )
