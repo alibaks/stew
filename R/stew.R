634a97912dd202c82974628cea76443fdcb374aa
alibakhs <sara.alibakhshi@gmail.com>
2023-10-13 20:36:06 +0300

    revise on pv_sig

--- R/stew.R
+++ R/stew.R
@@ -1,11 +1,10 @@
-
 # measure local indicators of spatial association (LISA) using the package elsa
-.getLisa <- function(x,d1,d2,stat) {
-  lisa(x,d1,d2,statistic = stat)
+.getLisa <- function(x, d1, d2, stat) {
+  elsa::lisa(x, d1, d2, statistic = stat)
 }
-#---
-.rasKendal <- function(x,pv_sig=0.05,...) {
-  # based on the mk.test function in the package trend
+
+# Calculate Kendall's Tau
+.rasKendal <- function(x, pv_sig, ...) {
   if (all(is.na(x))) return(NA)
   
   x <- x[!is.na(x)]
@@ -31,7 +30,6 @@
   sg <- sign(S)
   z <- sg * (abs(S) - 1) / sqrt(varS)
   
-  
   ## get the pvalue
   pval <- 2 * min(0.5, pnorm(abs(z), lower.tail=FALSE)) # two.sided
   
@@ -40,62 +38,51 @@
   }
   
   tau
-  
-}
-#--------
-if (!isGeneric("stew")) {
-  setGeneric("stew", function(x,stat,d,output,pv_sig,...)
-    standardGeneric("stew"))
 }
 
-
-
-
+# stew function for RasterStackBrickTS
 setMethod('stew', signature(x='RasterStackBrickTS'),
-          function(x,stat,d,output,pv_sig,...) {
+          function(x, stat, d, output, pv_sig, ...) {
             ind <- index(x)
-            
             k <- NULL
             
             if (missing(pv_sig)) pv_sig <- NULL
-
+            
             if (missing(stat)) stat <- 'stg'
             else stat <- tolower(stat)
-
+            
             xx <- x@raster
-
+            
             if (missing(d)) d <- max(res(xx)) * sqrt(2)
             
             if (missing(output)) output <- 'rts'
             else {
               if (!all(output %in% c('raster','rts','tau'))) {
-                warning('Not all the specified items in output is recognized, default "rts" is used')
+                warning('Not all the specified items in output are recognized, default "rts" is used')
                 output <- 'rts'
               }
             }
             
-            
-            
             o <- raster(xx[[1]])
-
+            
             if (stat %in% c('stg','geary','localgeary','c')) {
               for (i in 1:nlayers(xx)) {
-                o <- addLayer(o, .getLisa(xx[[i]],d1=0,d2=d,stat='c'))
+                o <- addLayer(o, .getLisa(xx[[i]], d1 = 0, d2 = d, stat = 'c'))
               }
             } else if (stat %in% c('stm','moran','localmoran','m','i')) {
               for (i in 1:nlayers(xx)) {
-                o <- addLayer(o, .getLisa(xx[[i]],d1=0,d2=d,stat='i'))
+                o <- addLayer(o, .getLisa(xx[[i]], d1 = 0, d2 = d, stat = 'i'))
               }
             }
-            #-------
+            
             if ('tau' %in% output) {
-              k <- calc(o,.rasKendal,pv_sig=pv_sig)
+              k <- calc(o, .rasKendal, pv_sig = pv_sig)
             }
-            #--------
+            
             if ('rts' %in% output) {
               o <- rts(o, ind)
             }
-            #--------
+            
             if (length(output) == 1) {
               if (output == 'tau') return(k)
               else return(o)
@@ -104,17 +91,15 @@ setMethod('stew', signature(x='RasterStackBrickTS'),
               if ('tau' %in% output) oo[['tau']] <- k
               if ('rts' %in% output) oo[['rts']] <- o
               else oo[['raster']] <- o
-              return (oo)
+              return(oo)
             }
-
           }
 )
-#------
 
+# stew function for SpatRasterTS
 setMethod('stew', signature(x='SpatRasterTS'),
-          function(x,stat,d,output,pv_sig,...) {
+          function(x, stat, d, output, pv_sig, ...) {
             ind <- index(x)
-            
             k <- NULL
             
             if (missing(pv_sig)) pv_sig <- NULL
@@ -129,33 +114,31 @@ setMethod('stew', signature(x='SpatRasterTS'),
             if (missing(output)) output <- 'rts'
             else {
               if (!all(output %in% c('raster','rts','tau'))) {
-                warning('Not all the specified items in output is recognized, default "rts" is used')
+                warning('Not all the specified items in output are recognized, default "rts" is used')
                 output <- 'rts'
               }
             }
-            
-            
             
             o <- rast(xx[[1]])
             
             if (stat %in% c('stg','geary','localgeary','c')) {
               for (i in 1:nlyr(xx)) {
-                o <- c(o, .getLisa(xx[[i]],d1=0,d2=d,stat='c'))
+                o <- c(o, .getLisa(xx[[i]], d1 = 0, d2 = d, stat = 'c'))
               }
             } else if (stat %in% c('stm','moran','localmoran','m','i')) {
               for (i in 1:nlyr(xx)) {
-                o <- c(o, .getLisa(xx[[i]],d1=0,d2=d,stat='i'))
+                o <- c(o, .getLisa(xx[[i]], d1 = 0, d2 = d, stat = 'i'))
               }
             }
-            #-------
+            
             if ('tau' %in% output) {
-                k <- app(rast(o),.rasKendal,pv_sig=pv_sig)
+              k <- app(rast(o), .rasKendal, pv_sig = pv_sig)
             }
-            #--------
-            if (c('rts') %in% output) {
+            
+            if ('rts' %in% output) {
               o <- rts(o, ind)
             }
-            #--------
+            
             if (length(output) == 1) {
               if (output == 'tau') return(k)
               else return(o)
@@ -164,8 +147,7 @@ setMethod('stew', signature(x='SpatRasterTS'),
               if ('tau' %in% output) oo[['tau']] <- k
               if ('rts' %in% output) oo[['rts']] <- o
               else oo[['raster']] <- o
-              return (oo)
+              return(oo)
             }
-            
           }
 )
